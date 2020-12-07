b25bc25582c0accd1e1f1bd16dfaf90c4f1dcefd
emreariyurek <emreariyurek@gmail.com>
2020-12-07 22:15:09 +0300

    add count number of teams solution

---
+++ cpp/array/1395 Count Number of Teams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int count = 0;
+        int n = rating.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k =  j + 1; k < n; k++) {
+                    if (rating[i] < rating[j] && rating[j] < rating[k])
+                        count++;
+                    if (rating[i] > rating[j] && rating[j] > rating[k])
+                        count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
