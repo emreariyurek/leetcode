cae675850afb4bce9679491489e9c38b1b49e284
emreariyurek <emreariyurek@gmail.com>
2021-01-25 16:26:44 +0300

    add balanced binary tree solution

---
+++ cpp/recursion/0110 Balanced Binary Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root)
+            return true;
+        
+        return dfs(root) != -1;
+    }
+    
+    int dfs(TreeNode *node) {
+        if (!node)
+            return 0;
+        
+        int l = dfs(node->left);
+        int r = dfs(node->right);
+        if (l == -1 || r == -1 || abs(l - r) > 1)
+            return -1;
+        return max(l, r) + 1;
+    }
+};
